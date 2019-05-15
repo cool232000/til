b7c81c3540c63666520b61f8a95e00af20171077
cool232000 <47887717+cool232000@users.noreply.github.com>
2019-05-15 23:33:00 +0900

    Update 0515_exercise.js

--- 0515_exercise.js
+++ 0515_exercise.js
@@ -3,8 +3,8 @@
 
 // 단, if문을 사용한 답과 3항 연산자를 사용하는 답 두가지를 제시하여야 한다.
 
-// if 문
 
+// if 문
 function evenOrOdd(num) {
   if (num % 2 === 0) {
     return 'Even';
@@ -14,15 +14,12 @@ function evenOrOdd(num) {
   }
 }
 
-console.log(evenOrOdd(2));
-console.log(evenOrOdd(3));
-console.log(evenOrOdd(1000));
-
 
 // 삼항 조건 연산자
 function evenOrOdd(num) {
   return num % 2 ? 'Odd' : 'Even';
 }
+
 
 console.log(evenOrOdd(2));
 console.log(evenOrOdd(3));
@@ -32,9 +29,7 @@ console.log(evenOrOdd(1000));
 
 // 2. 1 ~ 10,000의 숫자 중 8이 등장하는 횟수 구하기 (Google)
 // 1부터 10,000까지 8이라는 숫자가 총 몇번 나오는가? 이를 구하는 함수를 완성하라.
-
 // 단, 8이 포함되어 있는 숫자의 갯수를 카운팅 하는 것이 아니라 8이라는 숫자를 모두 카운팅 해야 한다. 예를 들어 8808은 3, 8888은 4로 카운팅 해야 한다.
-
 // (hint) 문자열 중 n번째에 있는 문자 : str.charAt(n) or str[n]
 
 // 강사님 코드
@@ -67,6 +62,7 @@ function getCount8 () {
   return sum;
 }
 
+
 console.log(getCount8()); // 4000
 
 // 1부터 10,000까지의 숫자를 문자열로 바꿔서
@@ -79,7 +75,6 @@ console.log(getCount8()); // 4000
 
 // 3. 문자열 다루기
 // alphaString46 함수는 문자열 s를 매개변수로 입력받는다. s의 길이가 4 ~ 6이고, 숫자로만 구성되어 있는지 확인하는 alphaString46 함수를 완성하라.
-
 // 예를 들어 s가 'a234'이면 false를 리턴하고 '1234'라면 true를 리턴한다.
 
 // 2중 if 문
@@ -95,7 +90,6 @@ function alphaString46(s) {
 
 
 // 삼항 조건 연산자
-
 function alphaString46(s) {
   return !(isNaN(s)) ? (s.length > 3 && s.length < 7) : false;
 }
