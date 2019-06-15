0d169fcda39665bee3fb3f098d8055266ebc21ea
cool232000 <cool232000@gmail.com>
2019-06-16 02:29:25 +0900

    counter ng3

---
+++ exercise/counter_ng3/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { IncreaseComponent } from './increase/increase.component';
+import { DecreaseComponent } from './decrease/decrease.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    IncreaseComponent,
+    DecreaseComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
