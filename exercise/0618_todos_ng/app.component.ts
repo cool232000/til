9938cb225bc0acc276bc8910ce058987365c88c8
cool232000 <cool232000@gmail.com>
2019-06-19 00:51:38 +0900

    todos ng ver

---
+++ exercise/0618_todos_ng/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <app-todos></app-todos>
+
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'todos-ng';
+}
