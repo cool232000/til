02ddf7059bcf8537f13d2977ee2262452838d635
YiJaeE <jinnn2181@gmail.com>
2020-03-12 00:59:06 +0900

    feat: linear search, binary search, bubble sort on javascript

---
+++ exercise/search&sort.js
@@ -0,0 +1,58 @@
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (target === array[i]) {
+      return i;
+    }
+  } return -1;
+}
+
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(linearSearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 0)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 7)); // 2
+
+
+function binarySearch(array, target) {
+  let start = 0;
+  let end = array.length - 1;
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (array[mid] < target) {
+      start = mid + 1;
+    } else if (array[mid] > target) {
+      end = mid - 1;
+    } else {
+      return mid;
+    }
+  } return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(binarySearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 0)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 7)); // -1
+
+
+function bubbleSort(array) {
+  const length = array.length - 1;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        const changeNum = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = changeNum;
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([2, 4, 5, 1, 3]));
+console.log(bubbleSort([5, 2, 1, 3, 4, 6])); // [1, 2, 3, 4, 5, 6]
+console.log(bubbleSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
