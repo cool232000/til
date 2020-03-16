b2109accaf1f00f511bc09bde0eed12b67bca054
YiJaeE <jinnn2181@gmail.com>
2020-03-16 20:11:25 +0900

    feat: count the monkeys.js

--- algorithm/CountTheMonkeys.js
+++ algorithm/CountTheMonkeys.js
@@ -7,5 +7,5 @@ function monkeyCount(n) {
   } return arr;
 }
 
-console.log(monkeyCount(10));
-console.log(monkeyCount(5));
+console.log(monkeyCount(10)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(monkeyCount(5)); // [1, 2, 3, 4, 5]
