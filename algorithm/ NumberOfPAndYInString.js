e23eea6e37122778d1e5607f52e7fa1a3d8233d4
YiJaeE <jinnn2181@gmail.com>
2020-03-16 22:27:25 +0900

    feat: number of P and Y in string.js

---
+++ algorithm/ NumberOfPAndYInString.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  let answer = true;
+  const str = s.toUpperCase();
+  const pLength = [...str].filter((string) => string === 'P').length;
+  const yLength = [...str].filter((string) => string === 'Y').length;
+  answer = pLength === yLength ? answer : false;
+  return answer;
+}
+
+console.log(solution('pPoooyY')); // true
+console.log(solution('Pyy')); // false
