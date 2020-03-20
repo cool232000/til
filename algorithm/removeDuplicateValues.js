79575d3fe6bc7a7c7e92906d3be3e6ea16828985
YiJaeE <jinnn2181@gmail.com>
2020-03-20 16:42:36 +0900

    feat: remove duplicate values.js

---
+++ algorithm/removeDuplicateValues.js
@@ -0,0 +1,15 @@
+// set 객체 사용하기
+function uniqSet(array) {
+  const arr = [...new Set(array)];
+  return arr;
+}
+
+console.log(uniqSet([2, 1, 2, 3, 4, 3, 4])); // [ 2, 1, 3, 4 ]
+
+
+// filter 메소드 사용하기
+function uniqFilter(array) {
+  return [...array].filter((item, idx) => array.indexOf(item) === idx);
+}
+
+console.log(uniqFilter([2, 1, 2, 3, 4, 3, 4])); // [ 2, 1, 3, 4 ]
