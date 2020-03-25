b0a55e0b1758c6e982ee93d2b90b6518478f2b59
YiJaeE <jinnn2181@gmail.com>
2020-03-25 14:34:55 +0900

    refector: add use reduce()

--- algorithm/removeDuplicateValues.js
+++ algorithm/removeDuplicateValues.js
@@ -2,7 +2,7 @@
 // 인수로 전달된 배열의 요소 중에서 중복된 요소를 제외하고 유니크한 요소만을 반환하는 함수를 작성하라.
 // for 문은 사용하지 않도록 하자.
 
-// set 객체 사용하기
+// set 객체
 function uniqSet(array) {
   const arr = [...new Set(array)];
   return arr;
@@ -11,9 +11,17 @@ function uniqSet(array) {
 console.log(uniqSet([2, 1, 2, 3, 4, 3, 4])); // [ 2, 1, 3, 4 ]
 
 
-// filter 메소드 사용하기
+// filter
 function uniqFilter(array) {
   return array.filter((item, idx) => array.indexOf(item) === idx);
 }
 
 console.log(uniqFilter([2, 1, 2, 3, 4, 3, 4])); // [ 2, 1, 3, 4 ]
+
+
+// reduce
+function uniqReduce(array) {
+  return array.reduce((acc, cur, i, arr) => arr.indexOf(cur) === i ? acc.concat(cur) : acc, []);
+}
+
+console.log(uniqReduce([2, 1, 2, 3, 4, 3, 4])); // [ 2, 1, 3, 4 ]
