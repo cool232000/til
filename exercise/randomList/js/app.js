705e700f95d848d1e38c23039f83cbfc0da60f1e
YiJaeE <jinnn2181@gmail.com>
2020-04-01 22:58:42 +0900

    refactor: fixed an issue where the list was continuously added

--- exercise/randomList/js/app.js
+++ exercise/randomList/js/app.js
@@ -2,26 +2,26 @@
 const shuffleArr = ['빨강', '주황', '노랑', '초록', '파랑', '남색', '보라'];
 
 // variable
+const $section = document.querySelector('section');
 const $button = document.querySelector('button');
-const $shuffleText = document.querySelector('.shuffle');
 
 // button click event
 $button.addEventListener('click', () => {
-  // create div to delete array
-  const $container = document.createElement('div');
-  const removeContainer = document.getElementById('container');
-  if (removeContainer !== null) $shuffleText.removeChild(removeContainer);
+  // create ul to delete array
+  const $listContainer = document.createElement('ul');
+  const $removeContainer = document.getElementById('container');
+  if ($removeContainer !== null) $section.removeChild($removeContainer);
 
   // shuffle array
   const shuffle = shuffleArr.sort(() => Math.random() - Math.random());
 
   // insert shuffle array into view
   [...shuffle].map((listText) => {
-    $container.setAttribute('id', 'container');
+    $listContainer.setAttribute('id', 'container');
     const $randomList = document.createElement('li');
     const $randomText = document.createTextNode(`${listText}`);
-    $container.appendChild($randomList);
-    return $randomList.appendChild($randomText);
+    $randomList.appendChild($randomText);
+    return $listContainer.appendChild($randomList);
   });
-  $shuffleText.appendChild($container);
+  $section.appendChild($listContainer);
 });
