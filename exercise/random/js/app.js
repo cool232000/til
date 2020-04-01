cdb2c7a1124e902c2cb8f6775b7d7b3425d47def
YiJaeE <jinnn2181@gmail.com>
2020-04-01 17:40:30 +0900

    refactor: Fixed an issue where the list was continuously added

--- exercise/random/js/app.js
+++ exercise/random/js/app.js
@@ -1,15 +1,21 @@
+// variable
 const $button = document.querySelector('button');
 const $shuffleText = document.querySelector('.shuffle');
 
+// array
 const shuffleArr = ['빨강', '주황', '노랑', '초록', '파랑', '남색', '보라'];
 
+// button click event
 $button.addEventListener('click', () => {
+  // create div to delete array
   const $container = document.createElement('div');
   const removeContainer = document.getElementById('container');
   if (removeContainer !== null) $shuffleText.removeChild(removeContainer);
 
+  // shuffle array
   const shuffle = shuffleArr.sort(() => Math.random() - Math.random());
 
+  // insert shuffle array into view
   [...shuffle].map((listText) => {
     $container.setAttribute('id', 'container');
     const $randomList = document.createElement('li');
