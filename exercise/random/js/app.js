30723b1108acd38aa1f3e2be220e05e9ccb7f2c2
YiJaeE <jinnn2181@gmail.com>
2020-04-01 14:15:13 +0900

    refactor: random list with create element method

--- exercise/random/js/app.js
+++ exercise/random/js/app.js
@@ -1,13 +1,24 @@
-const button = document.querySelector('button');
-const shuffleText = document.querySelector('.shuffle');
+const $button = document.querySelector('button');
+const $shuffleText = document.querySelector('.shuffle');
 
 const FDS15 = [ 'FastJ', '시멘틱을 Flex 해버렸지 뭐야', '디버깅', '미소', 'CloneAble', 'V' ];
 
-button.addEventListener('click', function() {
-  let shuffle = FDS15.sort(function() {
-    return Math.random() - Math.random();
+$button.addEventListener('click', () => {
+  let shuffle = FDS15.sort(() => Math.random() - Math.random());
+ 
+  [...shuffle].map((listText) => {
+    const $randomList = document.createElement('li');
+    $randomList.setAttribute('id', 'randomList');
+    const $randomText = document.createTextNode(`${listText}`)
+    $shuffleText.appendChild($randomList);
+    $randomList.appendChild($randomText);
   });
-  [...shuffle].map((text) => {
-    shuffleText.innerHTML += `<li>${text}</li>`;
-  });
+});
+
+$button.addEventListener('click', (e) => {
+  if (!!document.getElementsByTagName('li')) {
+    console.log(e.target.nextElementSibling);
+    const hi = document.getElementById('randomList');
+    console.log(hi);
+  }
 });
