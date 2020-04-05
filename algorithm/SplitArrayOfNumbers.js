926d770eab74dc1544abdf9fb70baa9c2f2d1aa1
YiJaeE <jinnn2181@gmail.com>
2020-04-05 23:30:08 +0900

    feat: split array of numbers.js

---
+++ algorithm/SplitArrayOfNumbers.js
@@ -0,0 +1,10 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12910
+
+function solution(arr, divisor) {
+  const sortArr = arr.sort((a, b) => a - b);
+  return sortArr.every((array) => array % divisor) ? [-1] : sortArr.filter((array) => array % divisor === 0);
+}
+
+console.log(solution([5, 9, 7, 10], 5));
+console.log(solution([2, 36, 1, 3], 1));
+console.log(solution([3, 2, 6], 10));
