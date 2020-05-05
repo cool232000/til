9ff1b847fb48f979cdeb88d72b1799acc2dd33d5
YiJaeE <jinnn2181@gmail.com>
2020-05-05 23:30:38 +0900

    exercise sql test 3

--- database/sql/test3.sql
+++ database/sql/test3.sql
@@ -4,9 +4,9 @@ USE world;
 SELECT name, population
 FROM country
 WHERE population > (
-	SELECT population
-	FROM country
-    WHERE name = 'mexico')
+  SELECT population
+  FROM country
+  WHERE name = 'mexico')
 ORDER BY population DESC;
 
 -- 2. 국가별 몇개의 도시가 있는지 조회하고 도시수 순으로 10위까지 내림차순하세요.
@@ -20,9 +20,9 @@ LIMIT 10;
 -- 3. 언어별 사용인구를 출력하고 언어 사용인구 순으로 10위까지 내림차순하세요.
 SELECT language, SUM(count) AS count
 FROM (
-	SELECT countrylanguage.language, CEILING(country.population * (countrylanguage.percentage * 0.01)) AS count
-	FROM countrylanguage, country
-	WHERE countrylanguage.countrycode = country.code
+  SELECT countrylanguage.language, CEILING(country.population * (countrylanguage.percentage * 0.01)) AS count
+  FROM countrylanguage, country
+  WHERE countrylanguage.countrycode = country.code
 ) as count
 GROUP BY language
 ORDER BY count DESC
