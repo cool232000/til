059253c76d0cad3dd0bf75c30aa2aec3ae8df303
YiJaeE <jinnn2181@gmail.com>
2020-05-07 13:01:41 +0900

    feat: login page on react

--- react/cp4jsx/src/App.js
+++ react/cp4jsx/src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  onChange = ({ target }) => {
+    if (target.matches(".id")) {
+      console.log("사용자 아이디 입력 중...");
+    } else {
+      console.log("사용자 패스워드 입력 중...");
+    }
+  };
+
+  onClick = () => {
+    console.log("로그인 시도");
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>LogIn</h1>
+        <form className="form-container">
+          <input
+            className="id"
+            placeholder="ID를 입력하세요"
+            onChange={this.onChange}
+          />
+          <input
+            className="pw"
+            placeholder="PW를 입력하세요"
+            onChange={this.onChange}
+          />
+          <button type="button" className="btn" onClick={this.onClick}>
+            LogIn
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
