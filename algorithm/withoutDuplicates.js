5ea0543c7f4819b2d116273e08f02536b05d6839
YiJaeE <jinnn2181@gmail.com>
2020-06-23 20:19:52 +0900

    feat: without duplicates number programmers coding test exam

--- algorithm/withoutDuplicates.js
+++ algorithm/withoutDuplicates.js
@@ -4,4 +4,5 @@ const notSameNum = arr => arr.filter((num, idx) => num !== arr[idx + 1]);
 
 console.log(notSameNum([1, 1, 3, 3, 0, 1, 1])); // [1, 3, 0, 1]
 console.log(notSameNum([4, 4, 4, 3, 3])); // [4, 3]
-console.log(notSameNum([1, 1])); // [4, 3]
+console.log(notSameNum([1, 1])); // [1]
+console.log(notSameNum([])); // []
