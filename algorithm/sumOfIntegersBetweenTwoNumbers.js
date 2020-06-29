d7cd19124a2fc8471570063821926ea3a3be8881
YiJaeE <jinnn2181@gmail.com>
2020-06-29 12:40:58 +0900

    feat: sum between two integers

--- algorithm/sumOfIntegersBetweenTwoNumbers.js
+++ algorithm/sumOfIntegersBetweenTwoNumbers.js
@@ -1,19 +1,23 @@
-// https://programmers.co.kr/learn/courses/30/lessons/12912
+/*
+https://programmers.co.kr/learn/courses/30/lessons/12912
 
-function sumOfInteger(a, b) {
-  let answer = 0;
-  if (a <= b) {
-    for (let i = a; i <= b; i++) {
-      answer += i;
-    }
-  } else {
-    for (let j = b; j <= a; j++) {
-      answer += j;
-    }
+21. 두 정수 사이의 합
+adder 함수는 정수 x, y를 인수로 전달받는다. 두 수와 두 수 사이에 있는 모든 정수를 더해서 리턴하도록 함수를 완성하라.
+x와 y가 같은 경우는 둘 중 아무 수나 리턴한다. x, y는 음수나 0, 양수일 수 있으며 둘의 대소 관계도 정해져 있지 않다.
+예를 들어 x가 3, y가 5이면 12를 리턴한다.
+*/
+
+const adder = (x, y) => {
+  const array = [];
+  if (x > y) {
+    const a = x;
+    x = y;
+    y = a;
   }
-  return answer;
-}
+  for (let i = x; i <= y; i++) array.push(i);
+  return array.reduce((acc, cur) => acc + cur, 0);
+};
 
-console.log(sumOfInteger(3, 5));
-console.log(sumOfInteger(3, 3));
-console.log(sumOfInteger(5, 3));
+console.log(adder(3, 5));
+console.log(adder(3, 3));
+console.log(adder(5, 3));
