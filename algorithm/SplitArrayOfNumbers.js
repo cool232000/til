186654ba8b73890fca852280bddc9b4ba9f0f166
YiJaeE <jinnn2181@gmail.com>
2020-10-05 23:42:20 +0900

    feat: split array of numbers

--- algorithm/SplitArrayOfNumbers.js
+++ algorithm/SplitArrayOfNumbers.js
@@ -2,9 +2,11 @@
 
 function solution(arr, divisor) {
   const sortArr = arr.sort((a, b) => a - b);
-  return sortArr.every((array) => array % divisor) ? [-1] : sortArr.filter((array) => array % divisor === 0);
+  return sortArr.every(array => array % divisor)
+    ? [-1]
+    : sortArr.filter(array => array % divisor === 0);
 }
 
-console.log(solution([5, 9, 7, 10], 5));
-console.log(solution([2, 36, 1, 3], 1));
-console.log(solution([3, 2, 6], 10));
+console.log(solution([5, 9, 7, 10], 5)); // [ 5, 10 ]
+console.log(solution([2, 36, 1, 3], 1)); // [ 1, 2, 3, 36 ]
+console.log(solution([3, 2, 6], 10)); // [ -1 ]
